334071944e7fd274e0a47c8a88d97447d381b440
OR-2000 <rina.onishi2951@gmail.com>
2025-04-18 10:41:54 +0900

    Update config files and modify .gitignore for local development

---
+++ exec.sh
@@ -0,0 +1,5 @@
+java -Dvertx.cacheDirBase=/tmp/$USER/ -jar out/FelisController/assembly.dest/out.jar config.json
+
+# java -jar out/FelisController/assembly.dest/out.jar config.json.local
+
+java -jar out/FelisExperiments/assembly.dest/out.jar runYcsb
